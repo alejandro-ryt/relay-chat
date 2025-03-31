847cc52e4b77a4caaabca41903b26881a9195b4a
Alejandro Solano <alejandro.sr@rytsolucion.com>
2025-03-31 15:42:53 -0600

    404 Page added, and base colors changed

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 const Layout = () => {
   return (
     <main className="grid place-items-center h-dvh max-w-[65vw] mx-auto">
-      <section className="bg-base-200 rounded-[1.5rem] w-full min-h-[75vh]">
+      <section className="bg-base-100 rounded-[1.5rem] w-full min-h-[75vh]">
         <Outlet />
       </section>
     </main>
