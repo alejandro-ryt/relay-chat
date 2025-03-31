52140f4297e7e6cc70e6b6741b6e5fe3b6cbd05b
Alejandro Solano <alejandro.sr@rytsolucion.com>
2025-03-31 15:53:08 -0600

    alias import added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router";
 import SignIn from "@/pages/SignIn";
 import SignUp from "@/pages/SignUp";
-import Layout from "./layout/Layout";
-import NotFound from "./pages/NotFound";
+import Layout from "@/layout/Layout";
+import NotFound from "@/pages/NotFound";
 
 const App = () => {
   return (
