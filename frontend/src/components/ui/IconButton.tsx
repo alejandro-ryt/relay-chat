7b1cba835eaf7a3bd7f83510eef8669a897ad952
Alejandro Solano <alejandro.sr@rytsolucion.com>
2025-03-31 17:40:39 -0600

    Checkpoint work in progress

--- frontend/src/components/ui/IconButton.tsx
+++ frontend/src/components/ui/IconButton.tsx
@@ -2,12 +2,15 @@ import { ReactNode } from "react";
 
 interface IconButtonProps {
   icon: ReactNode;
-  action: CallableFunction;
+  action: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
 const IconButton = ({ icon, action }: IconButtonProps) => {
   return (
-    <button className="btn btn-square m-8" onClick={() => action}>
+    <button
+      className="btn btn-square w-24 h-24 mx-3 my-0.5 bg-transparent border-none hover:bg-primary"
+      onClick={action}
+    >
       {icon}
     </button>
   );
--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,19 +1,24 @@
 import Avatar from "@/components/ui/Avatar";
 import IconButton from "@/components/ui/IconButton";
 import ChatIcon from "@/assets/ChatIcon";
+import FriendsIcon from "@/assets/FriendsIcon";
 
 const Chat = () => {
   return (
     <section className="grid grid-cols-12 grid-rows-10 gap-1 h-full rounded-[1.5rem]">
-      <section className="row-span-10 flex flex-col w-full h-full items-center py-6">
+      <section className="row-span-10 flex flex-col w-full h-full items-center justify-between pt-6">
         <Avatar />
 
         <div role="group">
           <IconButton icon={<ChatIcon />} action={() => console.log("test")} />
-          <IconButton icon={<ChatIcon />} action={() => console.log("test")} />
+          <IconButton
+            icon={<FriendsIcon />}
+            action={() => console.log("test")}
+          />
           <IconButton icon={<ChatIcon />} action={() => console.log("test")} />
           <IconButton icon={<ChatIcon />} action={() => console.log("test")} />
         </div>
+        <IconButton icon={<ChatIcon />} action={() => console.log("test")} />
       </section>
       <section className="col-span-2 row-span-10">8</section>
       <section className="col-span-9 row-span-10 col-start-4">9</section>
