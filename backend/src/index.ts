1e59e0e0622275d54dc9cb59f9404e1bbefe74a2
Jorge Gomez <Jorge.g@rytsolucion.com>
2025-04-01 17:37:11 -0600

    initial socket.io setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
 import express, { Express } from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import routes from "@/routes/index";
 import connectDB from "@/db/dbConfig";
 import { errorMiddleware } from "@/middlewares/errorMiddleware";
 const app: Express = express();
+const server = createServer(app);
+// Initialize socket
+const io = new Server(server);
 const PORT: number = 3001;
 
 // Connect to mongo
@@ -16,6 +21,11 @@ app.use("/api", routes);
 
 app.use(errorMiddleware);
 
-app.listen(PORT, () => {
+
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+});
+
+server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
