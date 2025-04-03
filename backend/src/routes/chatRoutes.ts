b92d371209c3e25f20a5ba9f7ba87bb6de34a85a
Jorge Gomez <Jorge.g@rytsolucion.com>
2025-04-03 13:33:06 -0600

    Create endpoint to fetch all the chats with a specific format by user id, and handle join, send message and disconnect events

---
+++ backend/src/routes/chatRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { getChatsByUserId } from "@/controllers/chatController";
+
+const router = Router();
+
+// Get chats with users 
+router.get("/by-user/:id", getChatsByUserId);
+
+export default router;
--- backend/src/services/chatService.ts
+++ backend/src/services/chatService.ts
@@ -5,49 +5,94 @@ import Chat from "@/models/chatModel";
 import Message from "@/models/messageModel";
 import { IUser, IUserDocument } from "@/interfaces/user";
 import { IChat, IChatService, IChatDocument } from "@/interfaces/chat";
-import { IMessageDocument } from "@/interfaces/message";
+import { IMessage, IMessageDocument } from "@/interfaces/message";
+import { Types } from "mongoose";
 
 const userService = new UserService();
 
 class ChatService implements IChatService {
-    // Find chat by name
-    public async findByChatName(chatName: string): Promise<IChatDocument | null> {
-        return await Chat.findOne({name: chatName}).exec();
-    }
-    // Find and/or save chat
-    public async saveChat(chatName: string, user: IUserDocument): Promise<IChatDocument> {
-        // Find chat or create a new one
-        const chat = await Chat.findOneAndUpdate(
-            { name: chatName },
-            { name: chatName, users: [user._id] },
-            { upsert: true, new: true }
-        ).exec();
+  // Get all chats with last message by user id
+  public async findChatsByUserId(userId: string) {
+    // Return the populated type directly
+    return await Chat.find({ members: userId })
+      .populate("members", "id firstName lastName username email")
+      .populate({
+        path: "messages",
+        options: { sort: { createdAt: -1 }, limit: 1 }, // Get the most recent message
+      })
+      .exec();
+  }
 
-        return chat;
+  // Find chat by name
+  public async findByChatName(chatName: string): Promise<IChatDocument | null> {
+    return await Chat.findOne({ name: chatName }).exec();
+  }
+  // Find and/or save chat
+  public async saveChat(
+    chatName: string,
+    type: "direct" | "group",
+    userId: string
+  ): Promise<IChatDocument> {
+    // Find chat by name
+    let chat = await Chat.findOne({ name: chatName }).exec();
+
+    // Find or save chat
+    if (chat) {
+      // Check if the user already joined the chat to avoid duplicates
+      const memberFound = chat.members.find((id) => id.toString() === userId);
+
+      if (!memberFound) {
+        // Add the user if not exist
+        const userIdObj = new Types.ObjectId(userId);
+        chat.members.push(userIdObj);
+        await chat.save();
+      }
+    } else {
+      // Create the chat
+      chat = await Chat.create({
+        name: chatName,
+        type,
+        members: [userId],
+        messages: [],
+      });
     }
+
+    return chat;
+  }
 
   // Save message in DB
-  public async saveMessage(chatId: string, message: string, userId: string): Promise<IMessageDocument> {
+  public async saveMessage(
+    message: string,
+    userId: string
+  ): Promise<IMessageDocument> {
     const newMessage = new Message({
-        chat: chatId,
-        username: userId,
-        message,
-        createdAt: new Date(),
+      username: userId,
+      message,
+      createdAt: new Date(),
     });
 
     return await newMessage.save();
   }
 
+  // Add the message reference (ID) to the messages array in the Chat model
+  public async addMessageToChat(
+    chat: IChatDocument,
+    messageId: string
+  ): Promise<void> {
+    // Push the new message ID into the messages array
+    const messageObjId = new Types.ObjectId(messageId);
+    chat.messages.push(messageObjId);
+
+    // Save the updated Chat document
+    await chat.save();
+  }
+
   // Service to handle user disconnecting
-  public async handleDisconnect(socket: Socket): Promise<void> {
-    // try {
-    //   const user = await User.findOneAndDelete({ socketId: socket.id });
-    //   if (user) {
-    //     console.log(`${user.username} disconnected`);
-    //   }
-    // } catch (error) {
-    //   console.error("Error handling disconnect:", error);
-    // }
+  public async handleDisconnect(
+    userId: string,
+    user: Partial<IUser>
+  ): Promise<IUser | null> {
+    return await userService.updateUser(userId, user);
   }
 }
 
